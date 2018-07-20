7b83797c6bdf9d073e4ccb1c20dee267dbd1223f
RECANATI Antoine <antoine.recanati@inria.fr>
2018-07-20 18:16:49 +0200

    modified proj2dupli.py

---
+++ source/gen_data.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Projects a matrix to the set of duplication constraints Z.T S Z = A
+"""
+import warnings
+import sys
+import numpy as np
+from scipy.sparse import coo_matrix
+
+
+def gen_dupl_mat(S, n_by_N, prop_dupli=1, rand_seed=1):
+    """
+    Generate observed matrix A, and duplication count C, and true assignment Z
+    from ground truth matrix S
+    """
+    (N, m) = S.shape
+    assert(N == m)
+
+    if n_by_N >= 1:
+        print("n_by_N({}) must be < 1. Setting to 1/2.".format(n_by_N))
+    n_ = int(N * n_by_N)
+
+    if prop_dupli > 1:
+        print("prop_dupli ({}) must be <= 1. Setting to 1.".format(prop_dupli))
+    n_dupli = int(n_ * prop_dupli)
+
+    # For reproductibility
+    np.random.seed(rand_seed)
+    # Just in case...
+    n_ = max(n_, 1)
+    n_dupli = max(n_dupli, 1)
+
+    n_add = N - n_
+    shuffled_N = np.random.permutation(N)
+    zis = np.zeros(0, dtype=int)
+    zjs = np.zeros(0, dtype=int)
+
+    # Add one element to each bin
+    zis = np.append(zis, np.arange(n_))
+    zjs = np.append(zjs, shuffled_N[:n_])
+    # Split the copy number surplus among the n_ nodes
+    which_selected = np.random.randint(0, high=n_, size=n_add)
+    zis = np.append(zis, which_selected)
+    zjs = np.append(zjs, shuffled_N[n_:])
+    # Build assignment matrix
+    Z = coo_matrix((np.ones(N), (zis, zjs)), shape=(n_, N), dtype=int)
+    # C = np.sum(Z, axis=1)
+    C = Z @ np.ones(N, dtype=int)
+    A = Z @ S @ Z.T
+
+    return(Z, A, C)
+
+
+
+
+if __name__ == 'main':
+
+    from mdso import SimilarityMatrix
+    import matplotlib.pyplot as plt
+
+    n = 200
+    type_noise = 'gaussian'
+    ampl_noise = 0.5
+    type_similarity = 'LinearStrongDecrease'
+    apply_perm = False
+    # Build data matrix
+    data_gen = SimilarityMatrix()
+    data_gen.gen_matrix(n, type_matrix=type_similarity, apply_perm=apply_perm,
+                        noise_ampl=ampl_noise, law=type_noise)
+    S = data_gen.sim_matrix
+
+    n_by_N = 0.7
+    prop_dupli = 0.9
+    rand_seed = 1
+
+    (Z, A, C) = gen_dupl_mat(S, n_by_N, prop_dupli=prop_dupli,
+                             rand_seed=rand_seed)
+    plt.matshow(S)
+    plt.show()
--- source/proj2dupli.py
+++ source/proj2dupli.py
@@ -6,6 +6,7 @@ Projects a matrix to the set of duplication constraints Z.T S Z = A
 import warnings
 import sys
 import numpy as np
+from scipy.sparse import find
 
 
 def one_proj_sorted(s_vec, a_val, u_b=None):
@@ -22,7 +23,6 @@ def one_proj_sorted(s_vec, a_val, u_b=None):
 
     if a_val > s_sum:
         # y = s + (a_val - s_sum) / p
-        # y += (a_val - s_sum) / p
         if u_b is not None:
             if y[0] > u_b:
                 above_bound = np.where(y > u_b)[0]
@@ -30,7 +30,7 @@ def one_proj_sorted(s_vec, a_val, u_b=None):
                     warnings.warn("pb. infeasible with upper bound {}".format(
                         u_b
                     ))
-                    y[:] = u_b
+                    y[above_bound] = u_b
                 else:
                     y[above_bound] = u_b
                     y[above_bound[-1]+1:] = one_proj_sorted(
@@ -48,15 +48,113 @@ def one_proj_sorted(s_vec, a_val, u_b=None):
     return(y[invperm])
 
 
-def proj_sparse():
+def one_proj_sparse(s_vec, a_val, u_b=None, k_sparse=None):
+    p = len(s_vec)
+    # s_sum = np.sum(s_vec)
+    perm = np.argsort(-s_vec)
+    invperm = np.argsort(perm)
+    s = s_vec[perm]
+    if k_sparse is not None:
+        if k_sparse < p:
+            yy = np.zeros(p)
+            yy[:k_sparse] = one_proj_sorted(s[:k_sparse], a_val,
+                                            u_b=u_b)
+            return(yy[invperm])
 
-    return
+    return(one_proj_sorted(s_vec, a_val, u_b=u_b))
+
+
+def proj2dupli(S, Z, A, u_b=None, k_sparse=None, include_main_diag=True):
+    """
+    projects the matrix S onto the set of matrices X such that
+    Z X Z.T = A
+    Z is n x N assignment matrix, with Z @ 1_N = C, and 1_n.T @ Z = 1_N
+    """
+    # Should check symmetry or triangularity and switch to lower triangular
+    if include_main_diag:
+        k = 0
+    else:
+        k = -1
+    (N, N1) = S.shape
+    assert(N == N1)
+    (n, N2) = Z.shape
+    assert(N2 == N)
+    (n1, n2) = A.shape
+    assert(n == n1 and n == n2)
+
+    Atri = np.tril(A, k=k)
+    (sis, sjs, svs) = find(Atri)
+    n_vals = len(sis)
+
+    (i_tril, j_tril) = np.tril_indices(N, k=k, m=N)
+    ind_tril = np.ravel_multi_index((i_tril, j_tril), (N, N))
+    tril_argsort = np.argsort(ind_tril)
+    tril_map = np.zeros(N**2, dtype=int)
+    tril_map[ind_tril] = tril_argsort
+    s_flat = S.flatten()[ind_tril]
+    s_new = np.zeros(len(ind_tril))
+
+    these_ks = [np.where(Z[ii])[0] for ii in sis]
+    these_ls = [np.where(Z[jj])[0] for jj in sjs]
+    # Stay in lower triangle
+    # the_ks = np.maximum(these_ks, these_ls)
+    # the_ls = np.minimum(these_ks, these_ls)
+    for k in range(n_vals):
+        # s_vec = S[these_ks, these_ls].flatten()
+        # if len(s_vec) == 1:
+        #     pass
+        # else:
+        my_ks = these_ks[k]
+        my_ls = these_ls[k]
+        my_ks, my_ls = np.meshgrid(these_ks[k], these_ls[k])
+        my_ks = my_ks.flatten()
+        my_ls = my_ls.flatten()
+        # Stay in lower triangle
+        the_ks = np.maximum(my_ks, my_ls)
+        the_ls = np.minimum(my_ks, my_ls)
+        these_ind = np.ravel_multi_index((the_ks, the_ls), (N, N))
+        these_ind = tril_map[these_ind]
+        # if len(these_ind) == 1:
+        if type(these_ind) is not np.ndarray:
+            s_new[these_ind] = svs[k]
+        else:
+            s_vec = s_flat[these_ind]
+            # Perform the projection for subscript pair (i,j)
+            s_new[these_ind] = one_proj_sparse(s_vec,
+                                               svs[k], u_b=None, k_sparse=None)
+
+    xnew = np.zeros(N**2)
+    xnew[ind_tril] = s_new
+    X_proj = np.reshape(xnew, (N, N))
+    if not include_main_diag:
+        X_proj += X_proj.T
+    else:
+        X_proj += np.tril(X_proj, k=-1).T
+
+    return(X_proj)
 
 
 if __name__ == 'main':
 
-    s_vec = np.array([1,3,5,10])
-    a_val = 20
-    u_b = 6
+    s_vec = np.array([1, 3, 12, 10])
+    a_val = 40
+    u_b = 12
+    s_param = 3
 
     yy = one_proj_sorted(s_vec, a_val, u_b=u_b)
+
+    yyy = one_proj_sparse(s_vec, a_val, u_b=u_b, k_sparse=s_param)
+
+    # S2 = S.copy()
+    # Noise = np.random.rand(N,N)
+    # Noise += Noise.T
+    # Noise *= 0.1 * S2.max()
+    # S2 = S2 + Noise
+    # plt.matshow(S2); plt.show()
+    # Sproj = proj2dupli(S2, Z, A, u_b=None, k_sparse=None, include_main_diag=True)
+    #
+    # fig, axes = plt.subplots(1, 3)
+    # axes[0].matshow(S)
+    # axes[1].matshow(Sproj)
+    # axes[2].matshow(S2)
+    # plt.show()
